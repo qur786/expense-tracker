99dd2279abb993067f0d10fab7a27980ecbd9c5e
Qurban <ahmad76qurban@gmail.com>
2022-03-01 20:58:42 +0530

    refactor: use custom date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import "./ExpenseItem.css";
 export const ExpenseItem = (props) => {
+    const { date } = props;
+    const month = date.toLocaleString("en-US", {month: "long"});
+    const day = date.toLocaleString("en-US", { day: "2-digit"});
+    const year = date.getFullYear();
     return (
         <div className="expense-item">
-            <h2>{props.date.toISOString()}</h2>
+            <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">&#8377; {props.amount}</div>
