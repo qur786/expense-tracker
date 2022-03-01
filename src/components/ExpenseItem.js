c34606a2247365238b6fa0418fdd462de5a1a3b4
Qurban <ahmad76qurban@gmail.com>
2022-03-01 21:28:50 +0530

    refactor: split expense date from expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,9 @@
+import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 export const ExpenseItem = (props) => {
-    const { date } = props;
-    const month = date.toLocaleString("en-US", { month: "long" });
-    const day = date.toLocaleString("en-US", { day: "2-digit" });
-    const year = date.getFullYear();
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">&#8377; {props.amount}</div>
