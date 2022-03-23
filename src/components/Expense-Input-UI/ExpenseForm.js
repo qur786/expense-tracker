3d5820ee1d04a6328249b223fff6a9599c9b770b
Qurban <ahmad76qurban@gmail.com>
2022-03-24 00:46:41 +0530

    add form submit handler

--- src/components/Expense-Input-UI/ExpenseForm.js
+++ src/components/Expense-Input-UI/ExpenseForm.js
@@ -1,25 +1,53 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
-export function ExpenseForm () {
+export function ExpenseForm() {
+    const [title, setTitle] = useState("");
+    const [amount, setAmount] = useState(0);
+    const [date, setDate] = useState(new Date().toISOString());
+    const titleChangeHandler = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const amountChangeHandler = (e) => {
+        setAmount(Number.parseInt(e.target.value));
+    }
+
+    const dateChangeHandler = (e) => {
+        setDate(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const expense = {
+            title,
+            amount,
+            date: new Date(date),
+        }
+        setTitle("");
+        setAmount(0);
+        setDate("");
+    }
+
     return (
-            <form className="new-expense">
-                <div className="new-expense__controls">
-                    <div class="new-expense__control">
-                        <label>Title</label>
-                        <input type="text" />
-                    </div>
-                    <div class="new-expense__control">
-                        <label>Amount</label>
-                        <input type="number" min="0" step="1" />
-                    </div>
-                    <div class="new-expense__control">
-                        <label>Date</label>
-                        <input type="date" max={new Date().toISOString().split("T")[0]} />
-                    </div>
-                    <div className="new-expense__actions">
-                        <button type="submit">Add Expense</button>
-                    </div>
+        <form className="new-expense" onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div class="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" value={title} onChange={titleChangeHandler} />
                 </div>
-            </form>
+                <div class="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" value={amount} min="0" step="1" onChange={amountChangeHandler} />
+                </div>
+                <div class="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" value={date} max={new Date().toISOString().split("T")[0]} onChange={dateChangeHandler} />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
     )
 }
