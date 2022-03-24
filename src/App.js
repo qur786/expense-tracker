74c71f028a49875bbb2dc614eab730ac74fa433a
Qurban <ahmad76qurban@gmail.com>
2022-03-24 21:31:34 +0530

    refactor: use expenses state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
+import { useState } from "react";
 import { Expenses } from "./components/Expense-UI/Expenses";
 import { ExpenseInput } from "./components/Expense-Input-UI/ExpenseInput";
 
 export function App() {
-    const expenses = [
-        {
-            id: Math.random().toString(),
-            title: "Car Wash",
-            amount: 500,
-            date: new Date(2021, 1, 20),
-        },
-        {
-            id: Math.random().toString(),
-            title: "Grossary",
-            amount: 3000,
-            date: new Date(2020, 11, 2),
-        },
-        {
-            id: Math.random().toString(),
-            title: "Bike Repair",
-            amount: 200,
-            date: new Date(2020, 10, 11),
-        },
-        {
-            d: Math.random().toString(),
-            title: "Furniture",
-            amount: 10000,
-            date: new Date(2021, 5, 4),
-        },
-    ];
-
+    const [expenses, setExpenses] = useState([]);
     const addExpenseHandler = (expense) => {
-        expenses.push(expense);
-        console.log(expenses)
+        setExpenses((prev) => {
+            return [...prev, expense];
+        })
     }
 
     return (
