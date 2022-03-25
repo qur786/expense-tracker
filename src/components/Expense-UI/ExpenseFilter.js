e79be0c2a86f5099c6d216c5a377571358d0934c
Qurban <ahmad76qurban@gmail.com>
2022-03-25 22:54:24 +0530

    style: add css to expense filter component

--- src/components/Expense-UI/ExpenseFilter.js
+++ src/components/Expense-UI/ExpenseFilter.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./ExpenseFilter.css";
 
 export function ExpenseFilter (props) {
     const [ selectedYear, setSelectedYear ] = useState("2020");
@@ -6,9 +7,9 @@ export function ExpenseFilter (props) {
         setSelectedYear(e.target.value);
         props.onYearChange(Number.parseInt(e.target.value, 10));
     }
-    return <div>
-        <p>Filter by year</p>
-        <select value={selectedYear} onChange={handleYearChange}>
+    return <div className="expenseFilter">
+        <p className="expenseFilter__title">Filter by year</p>
+        <select value={selectedYear} onChange={handleYearChange} className="expenseFilter__year">
             <option value="2020">2020</option>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
