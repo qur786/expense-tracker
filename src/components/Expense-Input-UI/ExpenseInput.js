fc35e6cba29f648198f06172cc06403bbd46966a
Qurban <ahmad76qurban@gmail.com>
2022-03-26 00:35:22 +0530

    refactor: add individual add expense and close button

--- src/components/Expense-Input-UI/ExpenseInput.js
+++ src/components/Expense-Input-UI/ExpenseInput.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { ExpenseForm } from "./ExpenseForm";
 import "./ExpenseInput.css";
 
 export function ExpenseInput(props) {
+    const [ openForm, setOpenForm ] = useState(false);
+    const handleOpenForm = () => setOpenForm(prev => !prev);
     const saveExpenseDataHandler = (expenseInput) => {
         const expense = {
             ...expenseInput,
@@ -11,7 +14,12 @@ export function ExpenseInput(props) {
     }
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {openForm === true 
+            ? <>
+            <button id="new-expense__close" onClick={handleOpenForm}>X</button>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> 
+            </>
+            : <button onClick={handleOpenForm}>Add Expense</button>}
         </div>
     );
 }
