4b87f85b382da430a7a956580914eb968861af65
Qurban <ahmad76qurban@gmail.com>
2022-03-26 22:18:14 +0530

    refactor: add app title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Expenses } from "./components/Expense-UI/Expenses";
 import { ExpenseInput } from "./components/Expense-Input-UI/ExpenseInput";
+import "./App.css";
 
 export function App() {
     const [expenses, setExpenses] = useState([]);
@@ -12,6 +13,7 @@ export function App() {
 
     return (
         <div>
+            <h1 className="app-title">Expense Tracker</h1>
             <ExpenseInput onAddExpense={addExpenseHandler} />
             <Expenses items={expenses} />
         </div>
